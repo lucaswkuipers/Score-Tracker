5d3d1e0142215e47bae2a19ac2d1c01eb27a9020
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2022-01-02 15:58:44 -0300

    Add Xcode project

---
+++ ScoreTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreTracker
+//
+//  Created by Lucas Kuipers on 02/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
