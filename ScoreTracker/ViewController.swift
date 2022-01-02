6881082848991344d485a513aef2d5775806f4c9
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2022-01-02 16:09:29 -0300

    Clean project

--- ScoreTracker/ViewController.swift
+++ ScoreTracker/ViewController.swift
@@ -1,19 +1,3 @@
-//
-//  ViewController.swift
-//  ScoreTracker
-//
-//  Created by Lucas Kuipers on 02/01/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+final class ViewController: UIViewController {}
