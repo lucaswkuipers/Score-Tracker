744b632c701ecb8445039228d3650c6eff056b5d
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2022-01-02 17:05:33 -0300

    Add view code

---
+++ ScoreTracker/ScoreComposer.swift
@@ -0,0 +1,8 @@
+//
+//  ScoreComposer.swift
+//  ScoreTracker
+//
+//  Created by Lucas Kuipers on 02/01/22.
+//
+
+import Foundation
---
+++ ScoreTracker/ScoreView.swift
@@ -0,0 +1,8 @@
+//
+//  ScoreView.swift
+//  ScoreTracker
+//
+//  Created by Lucas Kuipers on 02/01/22.
+//
+
+import Foundation
