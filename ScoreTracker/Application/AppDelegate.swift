32aec7233d0f863ec7a4938cecbc6f5fe15b4d84
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2022-01-03 02:34:03 -0300

    Add missing line

--- ScoreTracker/Application/AppDelegate.swift
+++ ScoreTracker/Application/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         configureTabBarAppearance()
         configureNavigationBarAppearance()
